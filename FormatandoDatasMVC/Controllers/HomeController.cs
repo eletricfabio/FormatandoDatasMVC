5f93c900f1011077fce0d9966c4e5076212e8f7f
fabio.vieira@getinfo.tech <tgfzEhjR>
2021-08-17 21:18:41 -0300

    Adicionar arquivos de projeto.

---
+++ FormatandoDatasMVC/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using FormatandoDatasMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormatandoDatasMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Funcionarios> funcionario = new List<Funcionarios>()
+            {
+                new Models.Funcionarios
+                {
+                    DataAdmissao = DateTime.Now,
+                    DataNascimento = Convert.ToDateTime("30/04/1982"),
+                    Notas = "Teste",
+                    Pais = "Brasil",
+                    TelefoneResidencial = "(000) 9.9999-9999"
+                },
+                new Models.Funcionarios
+                {
+                    DataAdmissao = DateTime.Now,
+                    DataNascimento = Convert.ToDateTime("30/04/1982"),
+                    Notas = "Teste2",
+                    Pais = "Brasil2",
+                    TelefoneResidencial = "(000) 9.9999-9999"
+                }
+            };
+            return View(funcionario);
+        }
+       
+        public List<Funcionarios> Lista()
+        {
+            List<Funcionarios> funcionario = new List<Funcionarios>()
+            {
+                new Models.Funcionarios
+                {
+                    DataAdmissao = DateTime.Now,
+                    DataNascimento = Convert.ToDateTime("30/04/1982"),
+                    Notas = "Teste",
+                    Pais = "Brasil",
+                    TelefoneResidencial = "(000) 9.9999-9999"
+                },
+                new Models.Funcionarios
+                {
+                    DataAdmissao = DateTime.Now,
+                    DataNascimento = Convert.ToDateTime("30/04/1982"),
+                    Notas = "Teste2",
+                    Pais = "Brasil2",
+                    TelefoneResidencial = "(000) 9.9999-9999"
+                }
+            };
+            return funcionario;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FormatandoDatasMVC/Models/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormatandoDatasMVC.Models
+{
+    public class Funcionarios
+    {
+        [Required]
+        [Display(Name = "Telefone")]
+        public string TelefoneResidencial { get; set; }
+
+        [Display(Name = "País")]
+        public string Pais { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DataNascimento { get; set; }
+
+        [Display(Name = "Data de Admissão")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DataAdmissao { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Notas { get; set; }
+    }
+}
